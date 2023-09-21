c3891285bd6f3afca39ba60559b5446ba950eb49
Endel Dreyer <endel.dreyer@gmail.com>
2023-09-21 20:34:37 -0300

    update package versions. room connection working

--- sdk7/src/index.ts
+++ sdk7/src/index.ts
@@ -1,12 +1,12 @@
 import "./polyfill";
 import { Client, Room } from "colyseus.js";
 
-const client = new Client("https://127.0.0.1:2567");
+const client = new Client("http://127.0.0.1:2567");
 
 export function main() {
   console.log("MAIN!");
 
-  client.joinOrCreate("my_room").then((room: Room) => {
+  client.joinOrCreate("my_room", { userData: { displayName: "Endel" } }).then((room: Room) => {
     console.log("Joined", room)
   }).catch((e) => {
     console.log("ERROR", e);
